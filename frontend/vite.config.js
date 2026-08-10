646de242f4b93212d60a252445f504599177d465
Karthik <karthiktangeti11@gmail.com>
2026-08-10 09:13:04 +0530

    vite updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  base: '/Shopper/',
+  base: '/github_analyazer/',
 
   server: {
     host: '0.0.0.0',
